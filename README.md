66ee8b957bc25e206c237158e7b112c07d2af04c
PixelNetNeon <neonisdaboi@outlook.com>
2024-07-15 04:39:12 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# sbbazaar
+this tool was built quickly to basically automatically take a snapshot of the Hypixel Skyblock bazaar API.
+
+how else am i suppost to explain this? anyways [go follow my furry account](https://twitter.com/bismuthCMYK).
---
+++ src/main.rs
@@ -0,0 +1,30 @@
+use std::{thread, time, fs};
+
+use std::time::SystemTime;
+use chrono::offset::Utc; use chrono::DateTime;use chrono::SecondsFormat;
+
+
+fn main() {
+    let mut count = 0u32;
+
+    let set_time = time::Duration::from_secs(3600);
+    
+    
+
+    // Infinite loop
+    loop {
+        //count += 1;
+        
+        //println!("{}", count);
+
+        let response = reqwest::blocking::get("https://api.hypixel.net/v2/skyblock/bazaar").unwrap();
+        //println!("{}", response.text().unwrap());
+
+        let now = Utc::now();
+        let str_timestamp =  format!("{}", now.timestamp());
+
+        let _ = fs::write("/home/bismuthneon/Bazaar/".to_owned() + &str_timestamp + ".json", response.text().unwrap());
+
+        thread::sleep(set_time);
+    }
+}
